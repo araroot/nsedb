d7f63408cd649be3aabcf0d825e0d151331a7970
Ravi Aranke <ravi.aranke@gmail.com>
2013-03-25 17:19:44 +0530

    kajor rewrite of db code

--- code/equity_db.py
+++ code/equity_db.py
@@ -4,78 +4,75 @@ import datetime
 import os
 import math
 
-def write_rows():
+def fill_fo_price_table(c, con, path):
     for filename in sorted(os.listdir(path)):
-        print filename
-
+        filedate = datetime.datetime.strptime(filename.split('.')[0], '%d%m%Y')
+        
+        is_updated = c.execute('select * from update_status where date=? and table_name=?',[filedate, 'fo_prices']).fetchone()
+        if is_updated: 
+            print 'fo_prices table is already up-to-date for ', filedate
+            continue
+        
         with open(path + filename, 'r') as f:
             reader = csv.reader(f)
-            headers = reader.next()
-            
+            try:
+                headers = reader.next()
+            except:
+                print '*** DEBUG, got an empty file *** ', filename, filedate
+                continue 
+
             for row in reader:
-                if row == []: continue
-                if row[1] != 'OPTSTK': continue
+                if row == [] or row[1] != 'OPTSTK': continue
 
-                row[0] = datetime.datetime.strptime(row[0], '%d-%b-%y')
+                try:
+                    row[0] = datetime.datetime.strptime(row[0], '%d-%b-%y')
+                except ValueError:
+                    row[0] = datetime.datetime.strptime(row[0], '%d-%b-%Y') # format changed in later  years
                 
-                float_indexes = [4]
+                c.execute('insert into fo_prices values (?,?,?,?)', [row[2], row[0], row[1], float(row[4])])
+        
+        print 'DEBUG: updated data for', filename, filedate
+        c.execute('insert into update_status values (?,?,?)', ['fo_prices', filedate, 1]) # This stmt is executed only once per file       
+        con.commit()
 
-                for i in float_indexes:
-                    row[i] = float(row[i])
-                
-                prev_records = c.execute('select * from equity where on_date=? and instrument=? and stock=? and price=?', [row[0], row[1], row[2], row[4]])
-                
-                if prev_records.fetchall() == []:
-                    c.execute('insert into equity values (?,?,?,?)', [row[0], row[1], row[2], row[4]])
-                    c.execute('insert into update_status values (?,?,?)', [row[0], 'fosettle', 1])
-
-def fo_settle():
-    stocks_list = []
-    rows = c.execute('select stock from equity')
-    
-    for row in rows:
-        stocks_list.append(row[0])
-
-    stocks_list = list(set(stocks_list))
-
-    for stock in stocks_list:
+def fill_returns_table(c):
+    sym_list = c.execute('select distinct symbol from fo_prices').fetchall()
+    for t in sym_list:
+        stock = t[0]
         print stock
-        rows = c.execute('select * from equity where stock=? order by on_date asc', [stock]).fetchall()
+        rows = c.execute('select * from fo_prices where symbol=? order by date asc', [stock]).fetchall()
 
         for i in range(1,len(rows)):
             raw_return = math.log(rows[i][3]) - math.log(rows[i-1][3])
             adj_return = raw_return
             adj_flag   = 0
 
-            c.execute('insert into fo_settle values (?,?,?,?,?)', [stock, rows[i][0], raw_return, adj_return, adj_flag])
+            c.execute('insert or replace into fo_returns values (?,?,?,?,?)', [stock, rows[i][1], raw_return, adj_return, adj_flag])
 
-def create_tables():
-    c.execute('''create table if not exists update_status (for_date timestamp, entry_type text, yes_or_no real)''')
-    c.execute('''create table if not exists fo_settle (stock text, for_date timestamp, raw_return real, adj_return real, adj_flag real)''')
-
-    c.execute('''create table if not exists equity (on_date timestamp, instrument text, stock text, price real)''')
-
-    c.execute('''create index if not exists date_index  on fo_settle(for_date)''')
-    c.execute('''create index if not exists stock_index on fo_settle(stock)''')
-
-    c.execute('''create index if not exists date_index  on equity(on_date)''')
-    c.execute('''create index if not exists stock_index on equity(stock)''')
-
-    c.execute('''create index if not exists date_index  on update_status(for_date)''')
-    c.execute('''create index if not exists entry_index on update_status(entry_type)''')
-
+def create_tables(c):
+    c.execute('''create table if not exists update_status (table_name text not null, date timestamp not null,  
+                status integer, PRIMARY KEY(table_name, date))''')
+    
+    c.execute('''create table if not exists fo_returns (symbol text not null, date timestamp not null, raw_return real, 
+                 adj_return real, adj_flag integer, PRIMARY KEY(symbol, date))''')
+    
+    c.execute('''create table if not exists fo_prices (symbol text not null, date timestamp not null, instrument text, 
+                 price real, PRIMARY KEY(symbol, date))''') 
+    
+   
+    
 if __name__ == '__main__':
     conn = sqlite3.connect('../data/equity.db', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
+    cur = conn.cursor()
 
-    today = datetime.datetime.now()
+    cur.execute('PRAGMA synchronous=OFF') # Make bulk inserts go faster
+    conn.commit()
+   
+    create_tables(cur)
 
-    create_tables()
-
-    path = '../data/equity/'
-
-    write_rows()
-    fo_settle()
+    dir_path = '../data/equity/'
+    fill_fo_price_table(cur, conn, dir_path)
+    fill_returns_table(cur)
 
     conn.commit()
     conn.close()
