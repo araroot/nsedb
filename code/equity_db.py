7d13aa0d34faaa7882096b5b9642996344090d98
Ravi Aranke <ravi.aranke@gmail.com>
2013-03-26 17:21:56 +0530

    added volatility data

--- code/equity_db.py
+++ code/equity_db.py
@@ -4,6 +4,49 @@ import datetime
 import os
 import math
 
+def skipheader(reader):
+    try:
+        headers = reader.next()
+        return True
+    except:
+        print '*** DEBUG, got an empty file *** ', filename, filedate
+        return False 
+
+def parsedate(dstamp):
+    try:
+        return datetime.datetime.strptime(dstamp, '%d-%b-%y')
+    except ValueError:
+        try:
+            return datetime.datetime.strptime(dstamp, '%d-%b-%Y') # format changed in later  years
+        except ValueError:
+            print 'DEBUG: failed to parse date given as ', dstamp
+            return None    
+
+def fill_fo_volt_table(c, con, path):
+    for filename in sorted(os.listdir(path)):
+        filedate = datetime.datetime.strptime(filename.split('.')[0], '%d%m%Y')
+        is_updated = c.execute('select * from update_status where date=? and table_name=?',[filedate, 'fo_volt']).fetchone()
+        if is_updated: 
+            print 'fo_volt table is already up-to-date for ', filedate
+            continue
+        
+        with open(path + filename, 'r') as f:
+            reader = csv.reader(f)
+            if not skipheader(reader): continue
+            
+            for row in reader:
+                if row == []: continue
+                pdate = parsedate(row[0])
+                if not pdate: continue
+
+                volt = float(row[-1])
+                if volt < 1.0: volt = volt * 100.0 # From 29 Mar 2011, they changed it
+                c.execute('insert into fo_volt values (?,?,?)', [row[1], pdate, volt])
+        
+        print 'DEBUG: updated fo_volt for', filename, filedate
+        c.execute('insert into update_status values (?,?,?)', ['fo_volt', filedate, 1]) # This stmt is executed only once per file       
+        con.commit()
+
 def fill_fo_price_table(c, con, path):
     for filename in sorted(os.listdir(path)):
         filedate = datetime.datetime.strptime(filename.split('.')[0], '%d%m%Y')
@@ -15,23 +58,16 @@ def fill_fo_price_table(c, con, path):
         
         with open(path + filename, 'r') as f:
             reader = csv.reader(f)
-            try:
-                headers = reader.next()
-            except:
-                print '*** DEBUG, got an empty file *** ', filename, filedate
-                continue 
-
+            if not skipheader(reader): continue
+            
             for row in reader:
                 if row == [] or row[1] != 'OPTSTK': continue
-
-                try:
-                    row[0] = datetime.datetime.strptime(row[0], '%d-%b-%y')
-                except ValueError:
-                    row[0] = datetime.datetime.strptime(row[0], '%d-%b-%Y') # format changed in later  years
+                pdate = parsedate(row[0])
+                if not pdate: continue
                 
-                c.execute('insert into fo_prices values (?,?,?,?)', [row[2], row[0], row[1], float(row[4])])
+                c.execute('insert into fo_prices values (?,?,?,?)', [row[2], pdate, row[1], float(row[4])])
         
-        print 'DEBUG: updated data for', filename, filedate
+        print 'DEBUG: updated fo_prices data for', filename, filedate
         c.execute('insert into update_status values (?,?,?)', ['fo_prices', filedate, 1]) # This stmt is executed only once per file       
         con.commit()
 
@@ -59,7 +95,9 @@ def create_tables(c):
     c.execute('''create table if not exists fo_prices (symbol text not null, date timestamp not null, instrument text, 
                  price real, PRIMARY KEY(symbol, date))''') 
     
-   
+    c.execute('''create table if not exists fo_volt (symbol text not null, date timestamp not null, 
+                 ann_volt real, PRIMARY KEY(symbol, date))''') 
+    
     
 if __name__ == '__main__':
     conn = sqlite3.connect('../data/equity.db', detect_types=sqlite3.PARSE_DECLTYPES)
@@ -70,10 +108,9 @@ if __name__ == '__main__':
    
     create_tables(cur)
 
-    dir_path = '../data/equity/'
-    fill_fo_price_table(cur, conn, dir_path)
-    fill_returns_table(cur)
+    #fill_fo_price_table(cur, conn, path ='../data/fosett/')
+    fill_fo_volt_table(cur, conn, path ='../data/fovolt/')
+    #fill_returns_table(cur)
 
     conn.commit()
     conn.close()
-        
--- code/equity_downloader.py
+++ code/equity_downloader.py
@@ -6,7 +6,7 @@ import csv
 import os
 import time
 
-def fetch_page(url):
+def fetch_handle(url):
     req = urllib2.Request(url)
 
     req.add_header('Accept', '*/*')
@@ -19,12 +19,8 @@ def fetch_page(url):
 def format_date(d):
     return d.strftime('%d%m%Y')
 
-def get_dates():
-    start_date = datetime.datetime(2003,1,1)
-    end_date   = datetime.datetime(2003,12,31)
-
+def get_dates(start_date = datetime.datetime(2013,1,1), end_date = datetime.datetime.now()):
     dates = []
-
     cur_date = start_date
 
     while cur_date <= end_date:
@@ -33,29 +29,34 @@ def get_dates():
 
     return dates
 
-def get_csv(cur_date):
-    file_name = format_date(cur_date) + '.csv'
+def get_csv(cur_date, filetype):
 
+    data_folder = '../data/' + filetype +'/'
+    file_name = format_date(cur_date) + '.csv'
     if file_name in os.listdir(data_folder): return
 
-    params = urllib.urlencode({'h_filetype': 'fosett', 'date': cur_date.strftime('%d-%m-%Y'), 'section': 'FO'})
-    response = fetch_page('http://www.nseindia.com/ArchieveSearch?%s' % params)
+    params = urllib.urlencode({'h_filetype': filetype, 'date': cur_date.strftime('%d-%m-%Y'), 'section': 'FO'})
+    response = fetch_handle('http://www.nseindia.com/ArchieveSearch?%s' % params).read()
     
     # probably a holiday, so we can ignore it
-    if 'No file found for specified date. Try another date.' in response.read(): return
+    if 'No file found for specified date. Try another date.' in response: return
 
-    time.sleep(2)
+    time.sleep(1)
 
-    handle = fetch_page('http://www.nseindia.com/archives/nsccl/sett/FOSett_prce_%s.csv' % format_date(cur_date))
+    if filetype == 'fosett' : urltail = 'sett/FOSett_prce_%s.csv' %format_date(cur_date)
+    if filetype == 'fovolt' : urltail = 'volt/FOVOLT_%s.csv' %format_date(cur_date)
+
+
+    handle = fetch_handle('http://www.nseindia.com/archives/nsccl/' + urltail)
     f = open(data_folder + file_name, 'w')
     f.write(handle.read())
     f.close()
 
 if __name__ == '__main__':
-    dates_list  = get_dates()
-    data_folder = '../data/equity/'
+    dates_list  = get_dates(start_date=datetime.datetime(2003,1,1), end_date = datetime.datetime.now())
     
     for cur_date in dates_list:
-        print 'Fetching csv for', cur_date
-        get_csv(cur_date)
+        print 'Fetching data for', cur_date
+        get_csv(cur_date, filetype='fovolt')
+        #get_csv(cur_date, filetype='fosett') # Temporary comment
         cur_date += datetime.timedelta(days=1)
